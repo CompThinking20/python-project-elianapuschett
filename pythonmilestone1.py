8526570ee0ee7640475d5329918fcdd9811ca5a3
elianapuschett <71983857+elianapuschett@users.noreply.github.com>
2020-12-11 17:23:36 -0500

    Update pythonmilestone1.py

--- pythonmilestone1.py
+++ pythonmilestone1.py
@@ -10,3 +10,20 @@ def create_poem(words):
   return write
   
 print create_poem(nouns)
+
+import random
+nouns = ["darling", "foe", "lover", "friend", "forest" "catheral" "song" "story" "city" "light"]
+
+def create_poem(words):
+    poem = ""
+    word_number = random.randint(1, 3)
+    for item in range(0, word_number):
+        word_to_pop = random.randint(0, len(words)-1)
+        poem += words.pop(word_to_pop) + ""
+    return poem
+
+print create_poem(nouns)
+
+#Here i used the text generator formula to start with the randomization of nouns.
+#I will continue this for other types of words, then shuffle it all togehter.
+#This method will randomly generate words from the above list, and I can also use it for others, like verbs.
